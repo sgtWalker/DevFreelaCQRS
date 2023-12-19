80b7dcf19a64e86a3f3488468dc1889bbdc80bdd
Diego Araujo <diego.araujo@totvs.com.br>
2023-12-19 18:02:05 -0300

    Inclusão da estrutura inicial da camada infrastructure e do dbcontext

---
+++ DevFreelaCQRS.Infrastructure/DevFreelaCQRSDbContext.cs
@@ -0,0 +1,16 @@
+﻿using DevFreelaCQRS.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DevFreelaCQRS.Infrastructure
+{
+    public class DevFreelaCQRSDbContext : DbContext
+    {
+        public DevFreelaCQRSDbContext(DbContextOptions<DevFreelaCQRSDbContext> options) : base(options) { }
+
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Skill> Skills { get; set; }
+        public DbSet<UserSkill> UserSkills { get; set; }
+        public DbSet<ProjectComment> ProjectComments { get; set; }
+    }
+}
