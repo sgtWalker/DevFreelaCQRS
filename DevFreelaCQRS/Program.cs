a3df6cbdaa731e216c9412036836cecd137456ee
Diego Araujo <diego.araujo@totvs.com.br>
2023-12-20 19:04:45 -0300

    preparação do projeto para criação e execução da migration

--- DevFreelaCQRS/Program.cs
+++ DevFreelaCQRS/Program.cs
@@ -1,3 +1,6 @@
+using DevFreelaCQRS.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var connectionString = builder.Configuration.GetConnectionString("DevFreelaCQRSConnection");
+builder.Services.AddDbContext<DevFreelaCQRSDbContext>(options => options.UseSqlServer(connectionString));
 
 var app = builder.Build();
 
