c76f6815e2d0a213b1b4df96db09fc9aac2b6ec6
Diego Marques Araujo <rzrsdiego@outlook.com>
2024-02-07 22:16:51 -0300

    implementação do primeiro teste unitário

---
+++ DevFreelaCQRS.UnitTests/Core/Entities/ProjectTests.cs
@@ -0,0 +1,28 @@
+﻿using DevFreelaCQRS.Core.Entities;
+using DevFreelaCQRS.Core.Enums;
+
+namespace DevFreelaCQRS.UnitTests.Core.Entities
+{
+    public class ProjectTests
+    {
+        [Fact]
+        public void TestIfProjectStartWorks()
+        {
+            var project = new Project("Project Name Test", "Description Test", Guid.NewGuid(), Guid.NewGuid(), 1000);
+
+            Assert.Equal(ProjectStatus.Created, project.Status);
+            Assert.Null(project.StartedAt);
+
+            Assert.NotNull(project.Title);
+            Assert.NotEmpty(project.Title);
+
+            Assert.NotNull(project.Description);
+            Assert.NotEmpty(project.Description);
+
+            project.Start();
+
+            Assert.Equal(ProjectStatus.InProgress, project.Status);
+            Assert.NotNull(project.StartedAt);
+        }
+    }
+}
