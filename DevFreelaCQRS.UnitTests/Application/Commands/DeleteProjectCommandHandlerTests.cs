425584c6c73796faeb6b5a11c5d83acfeb11345a
Diego Marques Araujo <rzrsdiego@outlook.com>
2024-02-21 00:39:52 -0300

    Implementação de mais testes

---
+++ DevFreelaCQRS.UnitTests/Application/Commands/DeleteProjectCommandHandlerTests.cs
@@ -0,0 +1,54 @@
+﻿using DevFreelaCQRS.Application.Commands.ProjectCommands.DeleteProject;
+using DevFreelaCQRS.Core.Repositories;
+using DevFreelaCQRS.UnitTests.Helpers;
+using Moq;
+
+namespace DevFreelaCQRS.UnitTests.Application.Commands
+{
+    public class DeleteProjectCommandHandlerTests
+    {
+        [Fact]
+        public async Task ThereIsAProject_Executed_DeleteTheProject()
+        {
+            //Arrange
+            var project = ProjectTestsHelper.GetProject();
+
+            var projectRepositoryMock = new Mock<IProjectRepository>();
+            projectRepositoryMock
+                .Setup(pr => pr.GetByIdAsync(project.Id).Result)
+                .Returns(project);
+
+            var deleteProjectCommand = new DeleteProjectCommand(project.Id);
+            var deleteProjectCommandHandler = new DeleteProjectCommandHandler(projectRepositoryMock.Object);
+
+            //Act
+            await deleteProjectCommandHandler.Handle(deleteProjectCommand, new CancellationToken());
+
+            //Assert
+            Assert.True(!project.Active);
+            Assert.True(project.DeletedAt.HasValue);
+        }
+
+        [Fact]
+        public async Task ThereIsNoProject_Executed_ReturnNull()
+        {
+            //Arrange
+            var fakeProjectId = Guid.NewGuid();
+            var project = ProjectTestsHelper.GetNullProject();
+
+            var projectRepositoryMock = new Mock<IProjectRepository>();
+            projectRepositoryMock
+                .Setup(pr => pr.GetByIdAsync(fakeProjectId).Result)
+                .Returns(project);
+
+            var deleteProjectCommand = new DeleteProjectCommand(fakeProjectId);
+            var deleteProjectCommandHandler = new DeleteProjectCommandHandler(projectRepositoryMock.Object);
+
+            //Act
+            await deleteProjectCommandHandler.Handle(deleteProjectCommand, new CancellationToken());
+
+            //Assert
+            Assert.Null(project);
+        }
+    }
+}
---
+++ DevFreelaCQRS.UnitTests/Application/Commands/FinishProjectCommandHandlerTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFreelaCQRS.UnitTests.Application.Commands
+{
+    public class FinishProjectCommandHandlerTests
+    {
+        [Fact]
+        public async Task ThereIsAProjectCreated_Executed_DoNotFinishProject()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+        }
+    }
+}
